c71ae416465f527883b21c707fe336b7b4ee619e
fasamii <fasami0512@gmail.com>
2024-08-29 02:25:24 +0200

    error fixing and styling

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ const panel = document.getElementById('calc_panel');
 const buttons = document.querySelectorAll('.calc_button');
 const calculate = document.getElementById('calc__buton_r');
 let calculation = '';
-document.getElementById('clear').addEventListener('click', () => {calculation = '   '});
-calculate.addEventListener('click', () => {
-    calculation = String(eval(calculation.replaceAll('X','*').replaceAll(':','/')))
-    console.log(calculation)
+document.getElementById('clear').addEventListener('click', () => {
+    calculation = '';
     panel.textContent = calculation;
+});
+calculate.addEventListener('click', () => {
+    if(calculation.length > 0){
+        calculation = String(eval(calculation.replaceAll('X','*').replaceAll(':','/')))
+        panel.textContent = calculation;
+    }
 })
 buttons.forEach((element) => {
     element.addEventListener('click', () => {
@@ -17,8 +21,8 @@ buttons.forEach((element) => {
 function addToCalculation(element){
     if(['X',':','+','-'].includes(element)){
         const last = calculation.slice(-1);
-        if(!['X',':','+','-'].includes(last)){
-            calculation += element;         
+        if((!['X',':','+','-'].includes(last))&&(calculation.length > 0 || element == '-')){
+            calculation += element;           
         }
     }else{
         calculation += element;     
